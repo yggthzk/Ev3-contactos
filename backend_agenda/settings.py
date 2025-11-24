0a60a8a354703e210a04e1d4bb6e713f85332bda
bgrey77 <bgreyesq@gmail.com>
2025-11-24 18:36:05 -0300

    despliegue cliente
    integracion jwt cors, todo redyy

--- backend_agenda/settings.py
+++ backend_agenda/settings.py
@@ -98,4 +98,18 @@ REST_FRAMEWORK = {
     ),
 }
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+    "http://127.0.0.1:8000",
+    "http://localhost:8000",#pa seguir lo que nos dice la  rubrica  papito bello
+    
+]
+
+
+STATIC_URL = 'static/'
+
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
