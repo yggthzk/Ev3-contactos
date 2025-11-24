458c0da8b8ff1b8b4a1d543a1e0a4b7f557b2038
bgrey77 <bgreyesq@gmail.com>
2025-11-24 20:45:12 -0300

    renderfi

--- contactos/serializers.py
+++ contactos/serializers.py
@@ -1,9 +1,5 @@
 from rest_framework import serializers
-from .models import Contacto
-#formato JSON
-
 from django.contrib.auth.models import User
-from rest_framework import serializers
 from .models import Contacto
 
 class UserSerializer(serializers.ModelSerializer):
@@ -12,15 +8,11 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['username', 'password', 'email']
         extra_kwargs = {'password': {'write_only': True}}
 
-    def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)#5
+    def create(self, validated_data):#
+        user = User.objects.create_user(**validated_data)
         return user
 
 class ContactoSerializer(serializers.ModelSerializer):
-    class Meta:#.
-        model = Contacto#s
-        fields = '__all__'
-class ContactoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contacto
-        fields = '__all__'#todos los campos del modelo
+        fields = '__all__'#trodo
