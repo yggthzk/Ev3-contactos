5941bbc3642d10dbd3d3b8f13942881c6adc3a32
bgrey77 <bgreyesq@gmail.com>
2025-11-24 20:53:59 -0300

    templates

--- contactos/urls.py
+++ contactos/urls.py
@@ -1,11 +1,15 @@
+from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import ContactoViewSet, RegisterView
-
-router = DefaultRouter()#la default 8000
-router.register(r'contactos', ContactoViewSet)
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('register/', RegisterView.as_view(), name='auth_register'),
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
+    path('admin/', admin.site.urls),
+    path('api/', include('contactos.urls')),#se me peldieron todo lo contato
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
