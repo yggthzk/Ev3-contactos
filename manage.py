d8cc25ecc177b48fe91e5dd3046577a2b79c90d9
bgrey77 <bgreyesq@gmail.com>
2025-11-24 21:17:53 -0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -2,10 +2,9 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+sys.setrecursionlimit(3000)
 
 def main():
-    """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend_agenda.settings')
     try:
         from django.core.management import execute_from_command_line
@@ -16,7 +15,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
